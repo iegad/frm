cb5f6adb4e525e499dfcf17ecc1a77f71b67d206
iegad <iegad@vip.qq.com>
2024-10-14 10:09:56 +0800

    Update request.go

--- web/request.go
+++ web/request.go
@@ -5,9 +5,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/gox/frm/log"
-	"github.com/gox/frm/utils"
 )
 
 type BasicRequest struct {
@@ -25,12 +25,43 @@ func PostJSON(url string, req, rsp any) error {
 	}
 	defer ret.Body.Close()
 
-	body, _ := io.ReadAll(ret.Body)
-	log.Debug(*utils.Bytes2Str(body))
-	err = json.Unmarshal(body, rsp)
+	body, err := io.ReadAll(ret.Body)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return json.Unmarshal(body, rsp)
+}
+
+func Post(url string, req string, rsp any) error {
+	log.Debug(url)
+	log.Debug(req)
+	ret, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(req))
+	if err != nil {
+		return err
+	}
+	defer ret.Body.Close()
+
+	body, err := io.ReadAll(ret.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Debug(string(body))
+	return json.Unmarshal(body, rsp)
+}
+
+func Get(url string, rsp any) error {
+	ret, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer ret.Body.Close()
+
+	body, err := io.ReadAll(ret.Body)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(body, rsp)
 }
