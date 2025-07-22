dc3f942d6e391c123f3a718edd6863f448decf33
jaxx <jaxshow22@gmail.com>
2025-07-22 21:15:13 +0800

    Update docker.go

--- dkex/docker.go
+++ dkex/docker.go
@@ -1,9 +1,13 @@
 package dkex
 
-import "github.com/docker/docker/client"
+import (
+	"time"
+
+	"github.com/docker/docker/client"
+)
 
 func NewDocker(host string) (*client.Client, error) {
-	c, err := client.NewClientWithOpts(client.WithHost(host), client.WithAPIVersionNegotiation())
+	c, err := client.NewClientWithOpts(client.WithHost(host), client.WithAPIVersionNegotiation(), client.WithTimeout(time.Second*5))
 	if err != nil {
 		return nil, err
 	}
